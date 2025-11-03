fc3f96ffeb4b2eaff1e410baf3ce4d98dbe3b02f
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2025-11-03 13:25:11 +0530

    Implement stream filtering example in streamMethod
    Added example of using filter method with streams.

--- Stream API/src/org/stream/api/streamMethod.java
+++ Stream API/src/org/stream/api/streamMethod.java
@@ -1,7 +1,20 @@
 package org.stream.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class streamMethod {
 	public static void main(String[] args) {
+		
+		// filter(Predicate)
+		//   boolean value function
+		
+		// map(function) - each element operation perform
+		
+		
+		List<String> names = List.of("shubham", "subhanshu", "Abhinav");
+		List<String> newNames = names.stream().filter(e -> e.startsWith("s")).collect(Collectors.toList());
+		System.out.println(newNames);
 		
 	}
 }
