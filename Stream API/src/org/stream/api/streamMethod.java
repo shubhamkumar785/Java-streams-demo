bdbb32770305ce99e4773a243aebe37c2df44817
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2025-11-03 13:35:47 +0530

    Add stream method to square numbers
    Added a new stream operation to square numbers.

--- Stream API/src/org/stream/api/streamMethod.java
+++ Stream API/src/org/stream/api/streamMethod.java
@@ -15,6 +15,12 @@ public class streamMethod {
 		List<String> names = List.of("shubham", "subhanshu", "Abhinav");
 		List<String> newNames = names.stream().filter(e -> e.startsWith("s")).collect(Collectors.toList());
 		System.out.println(newNames);
+
+		
+		List<Integer> numbers = List.of(2, 3, 4, 5, 6, 7, 8, 9);
+		List<Integer> newNumber = numbers.stream().map(i -> i * i).collect(Collectors.toList());
+		System.out.println(newNumber);
 		
 	}
 }
+
